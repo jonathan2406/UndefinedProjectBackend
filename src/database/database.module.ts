687f3f3d5c8496e70767e00f165d9f8b4de389d0
David <juandis0246@gmail.com>
2024-09-13 15:05:03 -0500

    Config firebase (1/3)

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,27 @@
 import { Module, Global } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Global()
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [
     {
-      provide: 'FIREBASE_ADMIN_TOKEN_HERE',
-      useFactory: () => {
-        const serviceAccount = require('../config/serviceAccountKey.json');
-
+      provide: 'FIREBASE_ADMIN_TOKEN',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
         return admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-          databaseURL: 'https://<STAR_ME_UP_PROJECT_ID_HERE>.firebaseio.com',
+          credential: admin.credential.cert({
+            projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+            privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
+            clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+          }),
+          databaseURL: `https://${configService.get<string>('FIREBASE_PROJECT_ID')}.firebaseio.com`,
         });
       },
     },
   ],
-  exports: ['FIREBASE_ADMIN_TOKEN_HERE'],
+  exports: ['FIREBASE_ADMIN_TOKEN'],
 })
 export class DatabaseModule {}
