1612220028f9f1691510880926d51feca76a4cc3
Juan Sebastian Garcia <juansgperez@gmail.com>
2024-09-26 13:18:19 -0500

    User entity fixed

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
 export class CreateUserDto {
     readonly name: string;
+    readonly available_lios: number;
     readonly password: string;
     readonly profile_picture_url: string;
     readonly role: string;
