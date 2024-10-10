7d68b9bf262acac6079ca85af9068b6ab67ad72e
unknown <NicolasBernal1>
2024-10-10 18:03:25 -0500

    Return in email controller changed

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Res } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { Response } from 'express';
 
 @Controller('email')
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post('send')
-  async sendMail(@Body() mailData: { to: string; subject: string; body: string }) {
-    await this.emailService.sendMail(mailData.to, mailData.subject, mailData.body);
-    return { message: 'Email sent successfully!' };
+  async sendMail(@Body() mailData: { to: string; subject: string; body: string }, @Res() res: Response) {
+    try {
+      await this.emailService.sendMail(mailData.to, mailData.subject, mailData.body);
+      return res.status(HttpStatus.OK).json({
+        message: 'Email sent successfully!',
+      });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+    }
   }
 }
