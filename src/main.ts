afd48f47ebf231644b3804bd85adf897aaa9bc34
David Torres <119999924+davzshaw@users.noreply.github.com>
2024-10-17 10:12:21 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
   const port = 3000;
 
   await app.listen(port);
