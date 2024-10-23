c235f4366b951a19fe92a16799bb43f0e179c6e1
David Torres <119999924+davzshaw@users.noreply.github.com>
2024-10-22 23:43:02 -0500

    Update create-post.dto.ts

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,15 +1,14 @@
-
 export class CreatePostDto {
-    userId: string; // ID del usuario que hace el post
-    content: string; // Contenido del post
-    commentCount: number; // Número de comentarios
-    comments: CommentDto[]; // Lista de comentarios
-    likesCount: number; // Contador de "me gusta"
+  userId: string; // ID del usuario que hace el post
+  content: string; // Contenido del post
+  commentCount: number; // Número de comentarios
+  comments: CommentDto[]; // Lista de comentarios
+  likesCount: number; // Contador de "me gusta"
   }
-  
-  // DTO para los comentarios dentro de un post
-  export class CommentDto {
-    content: string; // El contenido del comentario
-    authorId: string; // El ID del autor del comentario
+
+
+// DTO para los comentarios dentro de un post
+export class CommentDto {
+  content: string; // El contenido del comentario
+  authorId: string; // El ID del autor del comentario
   }
-¿
