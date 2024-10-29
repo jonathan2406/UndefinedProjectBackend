dc9413238c05eb41de22abd398ba74e889da80ca
David Torres <119999924+davzshaw@users.noreply.github.com>
2024-10-29 14:07:26 -0500

    beating up credentials required

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ["http://localhost:3000"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+    credentials: false,
   });
   const port = 3000;
 
