81072a2758200cb1d0a690f580cc52dad4c4dbb1
David Torres <juandis0246@gmail.com>
2024-11-02 20:43:37 -0500

    Find by email company

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -85,6 +85,18 @@ export class CompanyController {
     }
   }
 
+  @Get('/get/email')
+  async getByEmail(@Body() email: string, @Res() res: Response) {
+    this.logger.log('Get company by email request received for email: ' + email);
+    try {
+      const company = await this.companyService.getByEmail(email);
+      return res.status(HttpStatus.OK).json(company);
+    } catch (error) {
+      this.logger.error('Error getting company by email', error.stack);
+      return res.status(HttpStatus.NOT_FOUND).send(error.message);
+    }
+  }
+
   @Put('/:id/groups-users')
   async updateGroupsUsers(@Param('id') id: string, @Body() updateGroupsUsersDto: UpdateGroupsUsersDto, @Res() res: Response) {
     this.logger.log('Update groups and users request received for company ID: ' + id);
